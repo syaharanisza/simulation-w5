21121df4392f49bd82938fe01e0779e3a13bfa1d
syaharanisza <syaharaniszakiyah@gmail.com>
2023-09-21 19:50:36 +0700

    update project

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const http = require("http");
 const port = process.env.PORT || 3000
 
 http.createServer((req, res) => {
-    res.write("hai bang")
+    res.write("hai section jayapuraaa")
     res.end()
 }).listen(port, () => {
     console.log(`server running on localhost:${port}`)
